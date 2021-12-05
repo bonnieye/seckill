9ae152d7f53515757fd685c226e4cf23a8f0f879
daytoy314 <506463262@qq.com>
2021-12-05 21:54:57 +0800

    优化登录功能（未测试，未改bug）

--- src/main/java/com/example/seckill/controller/GoodsController.java
+++ src/main/java/com/example/seckill/controller/GoodsController.java
@@ -18,22 +18,19 @@ import javax.servlet.http.HttpSession;
 public class GoodsController {
     /**
      * 跳转登录页
-     *
-     * @return
      */
     @RequestMapping("/toList")
-    public String toLogin(HttpSession session, Model model,
-                          @CookieValue("userTicket") String ticket) {
+    public String toLogin(Model model, User user) {
         /**
          * 跳转到商品列表页
          */
-        if (StringUtils.isEmpty(ticket)) {
-            return "login";
-        }
-        User user = (User) session.getAttribute(ticket);
-        if (null == user) {
-            return "login";
-        }
+//        if (StringUtils.isEmpty(ticket)) {
+//            return "login";
+//        }
+//        User user = (User) session.getAttribute(ticket);
+//        if (null == user) {
+//            return "login";
+//        }
         model.addAttribute("user", user);
         return "goodsList";
     }
